2de78ce931ab90e7721ef8af47bfaa7ace7e2eec
bchiaramonte-ux <b.chiaramonte@alumno.etec.um.edu.ar>
2026-05-18 08:33:48 -0300

    persona

---
+++ app/src/main/kotlin/org/example/App.kt
@@ -0,0 +1,11 @@
+
+package org.example
+
+
+class Persona(val nombre: String, val apellido: String, val edad: Int) {
+    
+}
+fun main{
+    val pers=Persona("Bautista", "Chiaramonte", 16) 
+    println(pers)
+}
